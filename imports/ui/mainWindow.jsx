d25e36639e2b4bde8cad3ccb86479f9b9718a7fb
drgorb <micha.roon@managination.com>
2017-03-22 21:44:32 +0100

    one render

--- imports/ui/mainWindow.jsx
+++ imports/ui/mainWindow.jsx
@@ -6,40 +6,19 @@ import historic from './historic';
 import toolbar from './toolbar';
 
 
-const CompHistoric = function () {
-    render(
-        <historic /> ,document.getElementById('render-target')
-    );
-};
-
-const CompCard = function () {
-    render(
-        <card />, document.getElementById('render-target')
-    );
-};
-
-const CompToolbar = function () {
-    render(
-        <toolbar />, document.getElementById('render-target')
-    );
-};
-
-
 export default class MainToolbar extends React.PureComponent {
 
     constructor(props) {
         super(props);
     };
 
-
     render() {
         return(
            <div className="container">
-                <CompToolbar/>
-                <CompCard/>
-               <CompHistoric/>
+                <toolbar/>
+                <card/>
+               <historic/>
            </div>
-
         )
     };
 }
